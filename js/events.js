e25457c29bbfe75fe7091aa2f32950baacc5ca74
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-29 12:55:13 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,19 @@
 //define functions here
+function getIt() {
+    
+}
+
+function frameIt() {
+    
+}
+
+function pressIt() {
+    
+}
+
+function submitIt() {
+    
+}
 
 $(document).ready(function(){
 

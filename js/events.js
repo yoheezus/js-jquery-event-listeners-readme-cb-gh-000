8f3e4293b69fa56a4021a9580623a6554b883008
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-29 13:11:30 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -24,7 +24,7 @@ function pressIt() {
 
 function submitIt() {
     $("form").on("submit", () => {
-        alert("Your form is going to be submitted now");
+        alert("Your form is going to be submitted now.");
     });
     return;
 };
